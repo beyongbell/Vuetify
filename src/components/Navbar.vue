78382655b42c85536c3cca17a78a05b1bda024e2
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-01-25 01:30:00 +0700

    Finish Team

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,13 +6,34 @@
                <span class="font-weight-light">Tinnakorn</span>
                <span>VUETIFY</span>
            </v-toolbar-title>
-           <v-spacer></v-spacer>
-           <v-btn flat color="grey">
+            <v-spacer></v-spacer>
+            <v-menu offset-y>
+                <v-btn flat slot="activator" color="grey">
+                    <v-icon left>expand_more</v-icon>
+                    <span>Menu</span>
+                </v-btn>
+                <v-list>
+                    <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+                        <v-list-tile-title>{{ link.text }}</v-list-tile-title>
+                    </v-list-tile>
+                </v-list>
+            </v-menu>
+            <v-btn flat color="grey">
                <span>Sign Out</span>
                <v-icon right>exit_to_app</v-icon>
-           </v-btn>
+            </v-btn>
        </v-toolbar>
        <v-navigation-drawer app v-model="drawer" class="indigo">
+           <v-layout column align-center>
+               <v-flex class="mt-5">
+                   <v-avatar size="100">
+                       <img src="User.png" alt="">
+                   </v-avatar>
+                   <p class="white--text subheading mt-1 text-md-center">
+                       Tinnakorn
+                   </p>
+               </v-flex>
+           </v-layout>
            <v-list>
                <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
                    <v-list-tile-action>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -2,14 +2,47 @@
     <div class="team">
         <h1 class="subheading grey--text">Team</h1>
         <v-container class="my-5">
-            content
+            <v-layout row wrap>
+                <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+                    <v-card flat class="text-xs-center ma-3">
+                        <v-responsive class="pt-4">
+                            <v-avatar size="100" class="grey lighten-2">
+                                <img :src="person.avatar">
+                            </v-avatar>
+                        </v-responsive>
+                        <v-card-text>
+                            <div class="subheading">
+                                {{ person.name }}
+                            </div>
+                            <div class="grey--text">
+                                {{ person.role }}
+                            </div>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-btn flat color="grey">
+                                <v-icon small left>message</v-icon>
+                                <span>Message</span>
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>
         </v-container>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            team : [
+                {name: 'ASD', role: 'Programmer', avatar : "User.png" },
+                {name: 'QWE', role: 'Designer'  , avatar : "User.png" },
+                {name: 'ZXC', role: 'Graphic'   , avatar : "User.png" },
+                {name: 'DFG', role: 'Social'    , avatar : "User.png" },
+            ]
+        }
+    }
 }
 </script>
 
